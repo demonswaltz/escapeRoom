afa02236db20f3dca6036e17258f2904c1b70571
Jessica Duffy <jduffy@apld.local>
2017-03-13 15:08:51 -0500

    change php file

--- website/framework/my_result.php
+++ website/framework/my_result.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
 <html>
-	<?php echo $_POST ['my_value']; ?>
+<body>
+	<?php $favcolor= $_POST ['favcolor'];
+	switch($favcolor){
+		case "red":
+			include("red.html");
+			break
+		case "blue":
+			echo "Your favorite color is blue!";
+			break
+		default:
+			echo "Do you even know what a good color is??"
+			break
+	}
+	?>
+
+</body>
 </html>
